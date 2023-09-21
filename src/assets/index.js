9c523320764a1276eb02d1fffe190579be1c7067
zumaku <60200121101@uin-alauddin.ac.id>
2023-09-21 10:11:30 +0800

    menambahkan assets

---
+++ src/assets/index.js
@@ -0,0 +1,31 @@
+import hero from "./hero.svg"
+import money from "./money.svg"
+import users from "./users.png"
+import user1 from "./user1.png"
+import user2 from "./user2.png"
+import user3 from "./user3.png"
+import user4 from "./user4.png"
+import user5 from "./user5.png"
+import user6 from "./user6.png"
+import footer_logo from "./footer_logo.svg" 
+import about from "./about.svg"
+import mine from "./mine.png"
+import faq from "./faq.svg"
+import sign from "./sign.svg"
+
+export {
+    hero,
+    money,
+    users,
+    user1,
+    user2,
+    user3,
+    user4,
+    user5,
+    user6,
+    footer_logo,
+    about,
+    mine,
+    faq,
+    sign,
+}
