feb587cb14664c821f98ba19a98b0e011aa6f667
zumaku <60200121101@uin-alauddin.ac.id>
2023-09-25 21:05:02 +0800

    Menambahkan React Router Dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
 import { Navbar, Footer } from "./components"
 import { Home } from "./pages"
 
 function App() {
   return (
-    <>
+    <Router>
       <div className="mx-auto px-5 sl:px-20 max-w-[1440px]">
         <Navbar />
-        <Home />
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
       </div>
       <Footer />
-    </>
+    </Router>
   )
 }
 
