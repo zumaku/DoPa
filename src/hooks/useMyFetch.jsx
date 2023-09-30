658ecd6306903dc5b4b0b7d055533a976c49033c
zumaku <60200121101@uin-alauddin.ac.id>
2023-09-30 14:29:11 +0800

    Menambahkan hooks fetching data

---
+++ src/hooks/useMyFetch.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+const useMyFetch = (url) => {
+    const [data, setData] = useState(null)
+    const [isPanding, setIsPanding] = useState(true)
+    const [isErr, setErr] = useState(false)
+
+    useEffect( () => {
+        setTimeout(() => {
+            fetch(url)
+            .then(respont => {
+                if(!respont.ok) throw Error("Tidak bisa melakukan fetching ke " + url)
+                return respont.json()
+            })
+            .then(data => {
+                setData(data)
+                setIsPanding(false)
+                setErr(null)
+            })
+            .catch(err => {
+                setErr(err.message)
+                setIsPanding(false)
+            })
+        }, 1000)
+    }, [url] )
+    
+    return { data, isPanding, isErr };
+}
+
+export default useMyFetch;
