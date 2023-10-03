a9c4c2c7eb3c546263d51d68a8ff9ab9309aed4f
zumaku <60200121101@uin-alauddin.ac.id>
2023-10-03 16:40:35 +0800

    Menginstall Forebase

---
+++ src/firebase.config.jsx
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFireStore } from "@firebase/firestore"
+
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_API_FIREBASE,
+    authDomain: "dopa-4bfdc.firebaseapp.com",
+    projectId: "dopa-4bfdc",
+    storageBucket: "dopa-4bfdc.appspot.com",
+    messagingSenderId: "65039526404",
+    appId: "1:65039526404:web:1c6c8b67803174707f2351",
+    measurementId: "G-Z6RPK705EJ",
+};
+
+const app = initializeApp(firebaseConfig)
+export const db = getFireStore(app)
+
