17d9e75da02b6c583fb5d27c39393aae1148bc86
zumaku <60200121101@uin-alauddin.ac.id>
2023-10-09 21:10:49 +0800

    Contact berfungsi sempurna

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,29 @@
+import { useState } from "react"
+import { useMyCreate } from "../hooks"
 import { style } from "../style"
 import { contact } from "../constans"
+import { AiOutlineClose } from "react-icons/ai"
 
 export default function Contact() {
+    const [newMessage, setNewMessage] = useState({name: "", email: "", message: ""})
+    const { isCreating, error, createData } = useMyCreate("contact")
+    const [isSendSuccess, setIsSendSuccess] = useState(false)
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setNewMessage({ ...newMessage, [name]: value })
+    }
+
     
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        await createData(newMessage)
+        
+        // Jika berhasil mengirim pesan
+        setNewMessage({name: "", email: "", message: ""})
+        setIsSendSuccess(true)
+    }
+
     return (
         <div className="flex flex-col sm:flex-row items-start w-full py-10 sm:py-16">
             <div className="w-full mb-10">
@@ -23,16 +44,24 @@ export default function Contact() {
                     ))
                 }
             </div>
-            <form action="">
+            <form onSubmit={handleSubmit}>
+                {isSendSuccess && !error && (
+                    <div className="bg-secondary p-2 mb-5 rounded-md flex justify-between items-center">
+                        <p><span className="font-anltpB">Yoss..</span> Pengiriman Berhasil</p>
+                        <AiOutlineClose className="hover:cursor-pointer" onClick={() => setIsSendSuccess(false)} />
+                    </div>
+                )}
                 <label
                     htmlFor="name"
                     className="font-anltpB"
                 >Nama</label>
                 <input 
                     type="text"
-                    name="email"
+                    name="name"
                     id="name"
                     className="w-full bg-gray-200 px-5 py-3 rounded-lg mb-5 outline-none"
+                    value={newMessage.name}
+                    onChange={handleChange}
                     required
                 />
                 <label
@@ -40,10 +69,12 @@ export default function Contact() {
                     className="font-anltpB"
                 >Email</label>
                 <input 
-                    type="number"
+                    type="email"
                     name="email"
                     id="email"
                     className="w-full bg-gray-200 px-5 py-3 rounded-lg mb-5 outline-none"
+                    value={newMessage.email}
+                    onChange={handleChange}
                     required
                 />
                 <label
@@ -54,9 +85,13 @@ export default function Contact() {
                     name="message"
                     id="message"
                     className="w-full bg-gray-200 resize-y my-5 h-36 p-5 rounded-lg outline-none"
+                    value={newMessage.message}
+                    onChange={handleChange}
                     required
                 ></textarea>
-                <button className={style.btn}>Kirim</button>
+                <button type="submit" className={style.btn} disabled={isCreating}>
+                    {isCreating ? "Mengirim..." : "Kirim"}
+                </button>
             </form>
         </div>
     )
