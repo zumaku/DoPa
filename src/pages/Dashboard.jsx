a8ff711a7c22930d0aebcfc957375e395a91523f
zumaku <60200121101@uin-alauddin.ac.id>
2023-10-14 21:30:46 +0800

    FUngsional Authentication

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,17 +1,48 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { sign } from "../assets";
 import { SignIn, SignUp } from "../components";
 import { style } from "../style";
+import { auth } from "../firebase.config"
+import { SignOutBtn } from "../components";
 
 export default function Dashboard() {
     const [isRegistered, setIsRegistered] = useState(true);
+    const [user, setUser] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+
+    // =============================================================
+    // ==== Masiih Perlu Diperbaiki strukturnya ini Authentiksi ====
+    // =============================================================
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged((user) => {
+            setUser(user);
+            setIsLoading(false);
+        });
+
+        return () => unsubscribe();
+    }, []);
+
+    if (isLoading) {
+        // Menampilkan indikator loading sementara Firebase memeriksa status otentikasi
+        return <p>Loading...</p>;
+    }
+
+    if (user) {
+        // Jika pengguna telah login, Anda dapat menampilkan konten dashboard atau yang sesuai.
+        return (
+            <div>
+                <p>Telah Login</p>
+                <SignOutBtn />
+                {/* Tambahkan komponen dashboard atau yang sesuai di sini */}
+            </div>
+        );
+    }
 
     return (
-        // <div className="h-screen flex justify-center items-center">
-        //     <h1 className="text-6xl font-anltpB">Dashboard In Proggress</h1>
-        // </div>
         <div className="flex flex-col sm:flex-row items-start w-full py-10 sm:py-16">
-            {isRegistered ? (
+            {isRegistered && !isLoading && user == null ? (
                 <div>
                     <SignIn />
                     <p className="mt-5">
