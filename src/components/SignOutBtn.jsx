2ece74f4037fc2746e42bfc9f53614d49d00dffb
zumaku <60200121101@uin-alauddin.ac.id>
2023-10-15 09:05:42 +0800

    Redirect ke home saat logout

--- src/components/SignOutBtn.jsx
+++ src/components/SignOutBtn.jsx
@@ -1,10 +1,13 @@
 import { style } from "../style";
 import { auth } from "../firebase.config";
 import { signOut } from "firebase/auth";
+import { useHistory } from "react-router-dom"
 
 export default function SignOutBtn() {
+    const history = useHistory()
     const signout = async () => {
         await signOut(auth);
+        history.push("/")
     };
 
     return (
